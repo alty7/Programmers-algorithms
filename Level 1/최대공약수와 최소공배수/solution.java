a8d52a7d699c484ecb9e5db7dd1fc272f89e6e31
Joonsub_Kim <63627252+ssub-e@users.noreply.github.com>
2020-06-11 21:57:26 +0900

    Update solution.java

--- Level 1/최대공약수와 최소공배수/solution.java
+++ Level 1/최대공약수와 최소공배수/solution.java
@@ -4,10 +4,10 @@ class Solution {
         int gcd=0; // 최대공약수 초기화
         
         
-        for(int i = 1; i<=Math.max(n, m); i++){
-            if(Math.min(n,m)/i<1){
-                break;
-            }
+        for(int i = 1; i<=Math.min(n, m); i++){
+//             if(Math.min(n,m)/i<1){
+//                 break;
+//             } // i의 범위를 min으로 설정해주어서 존재할 이유가 없어짐.
             if(n%i==0 && m%i==0){
                 gcd=i;
             }
