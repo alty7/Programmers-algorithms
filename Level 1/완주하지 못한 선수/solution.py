311263d392def50fe9b61a38091c80875f768098
Joonsub_Kim <63627252+ssub-e@users.noreply.github.com>
2020-08-21 21:08:10 +0900

    Create solution.py

---
+++ Level 1/완주하지 못한 선수/solution.py
@@ -0,0 +1,8 @@
+def solution(participant, completion):
+    answer = ''
+    participant=sorted(participant)
+    completion=sorted(completion)
+    for p, c in zip(participant, completion):
+        if p != c:
+            return p
+    return participant[-1]
